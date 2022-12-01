0154ce87a837d6838bb36c827c7b0a51484d368f
juan.figueroa@notified.com <juan.figueroa@unosquare.com>
2022-12-01 11:36:57 -0600

    Deleted repos that doens't apport any valuable knowledge.

--- other_api/lib/other_api/mailer.ex
+++
@@ -1,3 +0,0 @@
-defmodule OtherApi.Mailer do
-  use Swoosh.Mailer, otp_app: :other_api
-end
--- other_api/lib/other_api/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule OtherApi.Repo do
-  use Ecto.Repo,
-    otp_app: :other_api,
-    adapter: Ecto.Adapters.Postgres
-end
